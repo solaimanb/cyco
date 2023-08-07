f581754bf82a8cd14271a73ccc1fa4e784c8dfce
solo <soloman.err@.com>
2023-08-07 15:06:44 +0600

    Layout: implimented root layout for overall system

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,8 @@
+import { Root } from "postcss";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {path: '/', element: <Root/>}
+])
+
+export default router;
