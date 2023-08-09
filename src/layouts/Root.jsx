d28b4540c9751cb670bacfbec426247a3e8fcb41
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-10 00:24:20 +0600

    Title changed in index.html, Root import changed, it was comming form postcss and now correct and import from ../layouts/Root and Auth provider muted, it was creating an error because of no authinfo

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -2,7 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
     return (
-        <div>
+        <div className="bg-gray-800 w-full">
+            <h3 className="text-center text-3xl text-red-900">Mooving to test forward</h3>
             <Outlet/>
         </div>
     );
