f2ced55fbaa3f735fea32702ebfb8115e7a8c9a5
Nimul99 <htnimul99@gmail.com>
2023-08-10 16:55:59 +0600

    Added Featured Movie Section

---
+++ src/components/MovoCard/MovoCard.jsx
@@ -0,0 +1,10 @@
+
+const MovoCard = (img) => {
+    return (
+        <div>
+            <img className="w-[240px] h-[331px]" src={img} alt="" />
+        </div>
+    );
+};
+
+export default MovoCard;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,17 @@
-
+import FeaturedMovo from "../FeatureMarquee/FeaturedMovo";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     return (
         <div className="bg-white h-screen">
-            <h1>Comming Soon</h1>
+            <div className="mx-48 bg-gray-800 px-14">
+            <div>
+            <h1 className="text-white">featured movies -</h1>
+        </div>
+                <Marquee >
+                    <FeaturedMovo/>
+                </Marquee>
+            </div>
         </div>
     );
 };
