eb91f3ad0e718d0d07cec688ff1c848915e0ffad
Nimul99 <htnimul99@gmail.com>
2023-08-10 17:39:46 +0600

    solve error

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
     return (
         <div className="bg-white h-screen">
             <div className="mx-48 bg-gray-800 px-14">
-            <div>
+          
             <h1 className="text-white">featured movies -</h1>
-        </div>
-                <Marquee >
+     
+                <Marquee>
                     <FeaturedMovo/>
                 </Marquee>
             </div>
