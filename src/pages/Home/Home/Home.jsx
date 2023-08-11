5b19f54b0b1e0779a0d6101e602e6ea50de2c70a
solo <soloman.err@.com>
2023-08-11 16:35:57 +0600

    Layout: broken layout modified

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,16 +1,18 @@
-import FeaturedMovo from "../FeatureMarquee/FeaturedMovo";
 import Marquee from "react-fast-marquee";
+import LiveTv from "../../../components/Live-tv/LiveTv";
+import FeaturedMovo from "../FeatureMarquee/FeaturedMovo";
 
 const Home = () => {
     return (
-        <div className="bg-white h-screen">
-            <div className="mx-48 bg-gray-800 px-14">
+        <div className="bg-black h-screen">
+            <div className="px-14">
           
             <h1 className="text-white">featured movies -</h1>
      
                 <Marquee>
                     <FeaturedMovo/>
                 </Marquee>
+                <LiveTv/>
             </div>
         </div>
     );
