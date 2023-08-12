4febe3dec6fbec9a892b50e9616be76640879ee2
solo <soloman.err@.com>
2023-08-12 08:25:59 +0600

    Scheme: Navbar UI modified

--- src/pages/Home/featuredMovies/FeaturedMovies.jsx
+++ src/pages/Home/featuredMovies/FeaturedMovies.jsx
@@ -16,13 +16,13 @@ const FeaturedMovies = () => {
 
   return (
     <div className="mx-5 py-5">
-      <div className="flex flex-row md:flex-row gap-4 md:gap-10">
+      <div className="flex flex-row md:flex-row gap-4 md:gap-8">
         {imageUrls.map((image, index) => (
-          <div key={index} className="w-full md:w-[160px] h-[250px]">
+          <div key={index} className="w-full md:w-[180px] h-[280px]">
             <img
               src={image}
               alt={`Kid ${index + 1}`}
-              className="w-full h-full object-cover rounded hover:brightness-110"
+              className="w-full h-full object-cover rounded-sm hover:brightness-110"
             />
           </div>
         ))}
