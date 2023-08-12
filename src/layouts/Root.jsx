e21dcaa44810cc9fab111bd8510cddec5bb9eede
Md Sarowar <mdsarowarhang@gmail.com>
2023-08-12 22:54:35 +0300

    add subscription and also make 3 card all data avalew

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,17 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Home/shared/Navbar/Navbar";
 import Footer from "../pages/Home/shared/Footer/Footer";
+import Subscription from "../components/Subscription/Subscription";
 
 
 const Root = () => {
     return (
         <div className="bg-black w-full">
             <Navbar/>
-            
+        
             <div className="min-h-[calc(100vh-160px)]">
             <Outlet/>
             </div>
-            
+            <Subscription/>
             <Footer/>
         </div>
     );
