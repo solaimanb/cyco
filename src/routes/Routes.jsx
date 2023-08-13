a25acb0b20c23a0cd68fe0860c0e65b31b81b600
Md Sarowar <mdsarowarhang@gmail.com>
2023-08-13 14:09:47 +0300

    add registration and login page ui degin

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home/Home";
+import Registration from "../pages/registration/Registration";
+import Login from "../pages/login/Login";
 
 const router = createBrowserRouter([
     {path: '/',
@@ -29,6 +31,14 @@ const router = createBrowserRouter([
         {
             path: "/podcast",
             element: <Home/>,
+        },
+        {
+            path:'/registration',
+            element:<Registration/>
+        },
+        {
+            path:'/login',
+            element:<Login/>
         }
      ]
     }
