6c903f18306358c8a81146468bcb4142f40ff2d0
Md Sarowar <mdsarowarhang@gmail.com>
2023-08-15 09:47:34 +0300

    add series pages ui

--- src/components/series/SeriesDetails.jsx
+++ src/components/series/SeriesDetails.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 
+import YouTubePlaylist from "@codesweetly/react-youtube-playlist";
 const SeriesDetails = () => {
-    return (
-        <div>
-           <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row">
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+        <YouTubePlaylist className='mx-auto z-10'
+            apiKey="AIzaSyAG0Lqn0DeDWDg3xG1oGdwKnf0hCQ6OBi8"
+            playlistId="PL1pf33qWCkmjWcACm71NzSkb72r6YtDon"
+            uniqueName="THIS_PLAYLIST_INSTANCE_NAME"
+          />
 
-  <iframe className='w-full lg:h-[530px] md:h-96 h-96'  src="https://www.youtube.com/embed/fxXNbh4CRdg" title="The 10 AI Tools That&#39;ll SUPERCHARGE Your Productivity" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
- 
-    
-    <div className='w-1/3 mx-auto'>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SeriesDetails;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -38,6 +38,7 @@ const router = createBrowserRouter([
       {
         path: "/series/seriesDetails",
         element: <SeriesDetails/>,
+        
       },
       {
         path: "/live-tv",
