02eb312c0a78ec67b4dafb79f70f6fe5d98a3c3d
Rihanul Islam <rihanthedev@gmail.com>
2023-08-16 03:19:19 +0600

    FQA added

--- src/pages/About-Us/About.jsx
+++ src/pages/About-Us/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook,FaGithub,FaInstagramSquare } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagramSquare } from "react-icons/fa";
 
 const About = () => {
     return (
@@ -21,9 +21,9 @@ const About = () => {
                             <h2 className="card-title">Soloman Badsha</h2>
                             <p>Team Leader at CYCO</p>
                             <div className="card-actions">
-                                <FaFacebook className='text-3xl'/>
-                                <FaGithub className='text-3xl'/>
-                                <FaInstagramSquare className='text-3xl'/>
+                                <FaFacebook className='text-3xl' />
+                                <FaGithub className='text-3xl' />
+                                <FaInstagramSquare className='text-3xl' />
                             </div>
                         </div>
                     </div>
@@ -36,9 +36,9 @@ const About = () => {
                             <h2 className="card-title">Kabir Shuvo</h2>
                             <p>Web Developer at CYCO</p>
                             <div className="card-actions">
-                                <FaFacebook className='text-3xl'/>
-                                <FaGithub className='text-3xl'/>
-                                <FaInstagramSquare className='text-3xl'/>
+                                <FaFacebook className='text-3xl' />
+                                <FaGithub className='text-3xl' />
+                                <FaInstagramSquare className='text-3xl' />
                             </div>
                         </div>
                     </div>
@@ -51,9 +51,9 @@ const About = () => {
                             <h2 className="card-title">Mohammad Sarowar</h2>
                             <p>Web Developer at CYCO</p>
                             <div className="card-actions">
-                                <FaFacebook className='text-3xl'/>
-                                <FaGithub className='text-3xl'/>
-                                <FaInstagramSquare className='text-3xl'/>
+                                <FaFacebook className='text-3xl' />
+                                <FaGithub className='text-3xl' />
+                                <FaInstagramSquare className='text-3xl' />
                             </div>
                         </div>
                     </div>
@@ -66,9 +66,9 @@ const About = () => {
                             <h2 className="card-title">Naimul Islam</h2>
                             <p>Web Developer at CYCO</p>
                             <div className="card-actions">
-                                <FaFacebook className='text-3xl'/>
-                                <FaGithub className='text-3xl'/>
-                                <FaInstagramSquare className='text-3xl'/>
+                                <FaFacebook className='text-3xl' />
+                                <FaGithub className='text-3xl' />
+                                <FaInstagramSquare className='text-3xl' />
                             </div>
                         </div>
                     </div>
@@ -81,16 +81,89 @@ const About = () => {
                             <h2 className="card-title">Raihanul Islam Sharif</h2>
                             <p>Web Developer at CYCO</p>
                             <div className="card-actions">
-                                <FaFacebook className='text-3xl'/>
-                                <FaGithub className='text-3xl'/>
-                                <FaInstagramSquare className='text-3xl'/>
+                                <FaFacebook className='text-3xl' />
+                                <FaGithub className='text-3xl' />
+                                <FaInstagramSquare className='text-3xl' />
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* Question Answer:  */}
                 <div>
-                    
+                    <h2 className='text-3xl text-center my-3'>Important Question And Answer (FQA)</h2>
+                    {/* <div tabIndex={0} className="collapse bg-primary text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                            
+                        </div>
+                        <div className="collapse-content">
+                            <p> </p>
+                        </div>
+                    </div> */}
+                    {/* 1 */}
+                    <div tabIndex={0} className="collapse text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        Transforming OTT with User-Centric Experience
+                        </div>
+                        <div className="collapse-content">
+                            <p>CYCO is on a mission to transform the Over-The-Top (OTT) platform landscape. Using the robust MERN stack (MongoDB, Express.js, React, Node.js). we're crafting a unique, user-centric entertainment experience.</p>
+                        </div>
+                    </div>
+                    {/* 2 */}
+                    <div tabIndex={0} className="collapse text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        Our Vision
+                        </div>
+                        <div className="collapse-content">
+                            <p>CYCO leverages modern tech for positive societal impact. Our OTT platform surpasses traditional entertainment by delivering high-quality content directly to personal devices.</p>
+                        </div>
+                    </div>
+                    {/* 3 */}
+                    <div tabIndex={0} className="collapse text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        Industry Insights
+                        </div>
+                        <div className="collapse-content">
+                            <p> Market analysis confirms the thriving future of OTT platforms. With smartphones and smart TVs ubiquitous, the demand for on-demand entertainment soars. CYCO is poised to meet these evolving needs.</p>
+                        </div>
+                    </div>
+                    {/* 4 */}
+                    <div tabIndex={0} className="collapse  text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        User-Centric Approach
+                        </div>
+                        <div className="collapse-content">
+                            <p> Differentiated by user-focused design, CYCO is more than content delivery. Our role-based system seamlessly shifts viewers to contributors. This iterative approach evolves alongside user engagement.</p>
+                        </div>
+                    </div>
+                    {/* 5 */}
+                    <div tabIndex={0} className="collapse  text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        Personalization at its Core
+                        </div>
+                        <div className="collapse-content">
+                            <p> Meticulously curated content, advanced search, and personalized recommendations define CYCO's video library. Categories, tags, and genres cater to diverse interests, offering an unmatched viewing experience.</p>
+                        </div>
+                    </div>
+                    {/* 5 */}
+                    <div tabIndex={0} className="collapse text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        Roadmap to Success
+                        </div>
+                        <div className="collapse-content">
+                            <p>Collaboration is key. The CYCO team's unity drives limitless potential. Attracting stakeholders who share our vision, we're redefining engaging and personalized OTT experiences.</p>
+                        </div>
+                    </div>
+                    {/* 6 */}
+                    <div tabIndex={0} className="collapse  text-primary-content focus:bg-secondary focus:text-secondary-content">
+                        <div className="collapse-title">
+                        Join the Revolution
+                        </div>
+                        <div className="collapse-content">
+                            <p>CYCO is a tech-driven mission to reshape entertainment for the better. Embracing innovation, collaboration, and tech's power for positive change, we invite you to join us in revolutionizing OTT platforms.
+
+In search of a sponsor, donor, or investor to support our vision. With unwavering belief in our plan's potential, we welcome your valuable suggestions as we forge ahead.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
