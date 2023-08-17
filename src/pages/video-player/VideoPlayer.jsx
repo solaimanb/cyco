2562c003b0252300ddcc202233b4cb6c59a956af
Rihanul Islam <rihanthedev@gmail.com>
2023-08-17 17:16:24 +0600

    Video deatils and Video Player added

--- src/pages/video-player/VideoPlayer.jsx
+++ src/pages/video-player/VideoPlayer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const VideoPlayer = () => {
+    // Task: if we add all movie Link with Json file then we can show dynamic
+    // That's static video player
     return (
-        <div>
-            <p>wait wait video colter</p>
+        <div className="bg-gray-800 rounded-lg p-2">
+            <iframe
+                src="https://www.youtube.com/embed/atnW4D6pc-w"
+                title="Live Video"
+                allowFullScreen
+                className="w-full h-[500px] "
+            ></iframe>
         </div>
     );
 };
