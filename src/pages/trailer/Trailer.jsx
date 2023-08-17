5eb093d0767256909ef87088fd01bc21ac273bd6
solo <soloman.err@.com>
2023-08-18 03:01:54 +0600

    Core: movies data fetching error resolved

--- src/pages/trailer/Trailer.jsx
+++ src/pages/trailer/Trailer.jsx
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Loading from '../../components/loading/Loading';
 import TrailerCard from '../../components/trailerCard/TrailerCard';
+import useMovies from '../../hooks/useMovies';
 
 const Trailer = () => {
-    const [movies, setMovies] = useState([]);
+  const [movies, loading] = useMovies();
+  console.log(movies);
 
-    useEffect(() => {
-        fetch('/MoviesWithDetails.json') 
-            .then(res => res.json())
-            .then(data => setMovies(data))
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
-    return (
-        <div className="w-full flex flex-wrap gap-1">
-            {movies.map((movie, index) => (
-                <TrailerCard
-                key={index} 
-                movie={movie}
-                ></TrailerCard>
-            ))}
-        </div>
-    );
+//   useEffect(() => {
+//     fetch('/MoviesWithDetails.json')
+//       .then((res) => res.json())
+//       .then((data) => setMovies(data))
+//       .catch((error) => console.error('Error fetching data:', error));
+//   }, []);
+if (loading) {
+    // You can display a loading indicator here
+    return <Loading/>;
+  }
+
+  if (!Array.isArray(movies)) {
+    return <div>Error: Movies data is not an array.</div>;
+  }
+
+  return (
+    <div className="w-full flex flex-wrap gap-1">
+      {movies.map((movie, index) => (
+        <TrailerCard key={index} movie={movie}></TrailerCard>
+      ))}
+    </div>
+  );
 };
 
 export default Trailer;
