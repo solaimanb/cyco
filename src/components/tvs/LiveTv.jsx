c7fdec3953e47242920d8c831428dad8e5389950
Md Sarowar <mdsarowarhang@gmail.com>
2023-08-18 10:56:58 +0300

    Add updates to the live TV section and fix click functionality

--- src/components/tvs/LiveTv.jsx
+++ src/components/tvs/LiveTv.jsx
@@ -4,7 +4,7 @@ import { Link,} from 'react-router-dom';
 const LiveTv = ({data}) => {
  
     return (
-        <Link to={`live/${data?.id}`}> 
+        <Link to={`/live-tv/live/${data?.id}`}> 
          <div className="avatar flex flex-col items-center gap-4 text-white font-bold mx-auto">
         <div className="w-24 h-2w-24 rounded-full">
           <img src={data?.image} />
