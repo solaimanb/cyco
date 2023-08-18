9daaac30e1aa25bc01aa7653198bc774238a1341
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-18 15:29:28 +0600

    About us page updated, trying to be looks little bit better

--- src/components/paginaition/Pagination.jsx
+++ src/components/paginaition/Pagination.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination = ({ currentPage, totalPages, onPageChange, onNumberClick }) => {
   const isFirstPage = currentPage === 1;
   const isLastPage = currentPage === totalPages;
 
