bb4455f8e87ded52bc2a3ba1335ba676f7d09fcc
solo <soloman.err@.com>
2023-08-19 23:30:37 +0600

    Layout: 'Live Tv' pages UI modified

--- src/components/tvs/LiveVideo.jsx
+++ src/components/tvs/LiveVideo.jsx
@@ -1,38 +1,41 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
-import YouTubePlaylist from "@codesweetly/react-youtube-playlist";
-import CommentPage from "./CommentPage";
-import Tvs from "./Tvs";
+import React from 'react';
+import ReactPlayer from 'react-player';
+import { useLoaderData } from 'react-router-dom';
+import CommentPage from './CommentPage';
+import Tvs from './Tvs';
+
 const LiveVideo = () => {
   const tvData = useLoaderData();
 
   return (
     <>
-     
       <div>
-      <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row">
-    <iframe
-        className="mx-auto w-96 lg:w-full"
-          width="660"
-          height="515"
-          src={tvData[0]?.tvUrl}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-    <div className="w-1/3 mx-auto">
-    <CommentPage/>
-  
-    </div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row">
+            {/* <iframe
+              className="mx-auto w-96 lg:w-full"
+              width="660"
+              height="515"
+              src={tvData[0]?.tvUrl}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe> */}
 
-  </div>
-</div>
-     
- <Tvs/>
-      
-       
+            <ReactPlayer
+              url={movie?.Trailer?.Source}
+              width={'70%'}
+              height={'70%'}
+              controls
+            />
+            <div className="w-1/3 mx-auto">
+              <CommentPage />
+            </div>
+          </div>
+        </div>
+
+        <Tvs />
       </div>
     </>
   );
