fe5517e1da564253eacc03c830aaa1fec164f01b
solo <soloman.err@.com>
2023-08-22 14:28:21 +0600

    Core: resolved 'series' pages conflict

--- src/components/series/SeriesParts.jsx
+++ src/components/series/SeriesParts.jsx
@@ -1,16 +1,17 @@
+import YouTubePlaylist from '@codesweetly/react-youtube-playlist';
 import React from 'react';
 
 const SeriesParts = () => {
   return (
     <div>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
-          {/* <YouTubePlaylist
+          <YouTubePlaylist
             className="mx-auto z-10"
             apiKey="AIzaSyAG0Lqn0DeDWDg3xG1oGdwKnf0hCQ6OBi8"
             playlistId="PL1pf33qWCkmjWcACm71NzSkb72r6YtDon"
             uniqueName="THIS_PLAYLIST_INSTANCE_NAME"
-          /> */}
+          />
         </div>
       </div>
     </div>
