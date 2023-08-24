3c938b1b83c657d2a5801f36d7840312ec0c36e5
solo <soloman.err@.com>
2023-08-24 18:24:38 +0600

    Layout: Popular TVs player updated

--- src/pages/Home/popularTvs/populat-tv-player/PopularVideoPlayer.jsx
+++ src/pages/Home/popularTvs/populat-tv-player/PopularVideoPlayer.jsx
@@ -1,35 +1,23 @@
-import React from 'react'
-import ReactPlayer from 'react-player';
-
-
+import React from 'react';
 
 const PopularVideoPlayer = () => {
-
-
-
- const videoId = data?.tvUrl;
- console.log(videoId);
-
-
+  const videoId = data?.tvUrl;
+  console.log(videoId);
 
   return (
     <div className="fixed inset-0 flex justify-center items-center backdrop-blur-2xl">
       <ReactPlayer
-        url={
-          'ihave to show data.tvUrl here'
-        }
-        width={'60%'}
+        url={'ihave to show data.tvUrl here'}
+        width={'90%'}
         controls
       />
+
     </div>
-  )
-}
+  );
+};
 
-export default PopularVideoPlayer
-
-
-
+export default PopularVideoPlayer;
 
 //   ------------------
-  
+
 //   {videoId && <PopularVideoPlayer videoId={videoId} />}
