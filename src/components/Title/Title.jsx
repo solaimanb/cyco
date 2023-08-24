f26537b2cf4a889922081ea125bd42b2a411279c
solo <soloman.err@.com>
2023-08-24 15:25:22 +0600

    Layout: Homepage layout modified

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({title}) => {
     return (
-        <h1 className='border-l-4 pl-3 text-white text-xl font-bold my-4'> {title}</h1>
+        <h1 className='border-l-4 border-[#800000] pl-3 text-white text-xl font-bold my-4'> {title}</h1>
     );
 };
 
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -8,7 +8,7 @@ const Root = () => {
       <Navbar />
 
       <div className="min-h-[calc(100vh-160px)]">
-        <div className="w-[90%] mx-auto">
+        <div className="w-[95%] mx-auto">
           <Outlet />
         </div>
       </div>
