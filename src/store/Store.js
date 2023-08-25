21718d061562cc6861b3c60a4d641b2615173a2f
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-25 19:26:45 +0600

    UserSlice created and imported it to store the user reducer, Now I'm going to use the reducer..

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import homeSlice from './homeSlice/homeSlice';
+import userReducer from './userSlice/userSlice';
+
+
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+
 
 const store = configureStore({
   reducer: {
     home: homeSlice,
+    users: rootReducer,
   },
 });
 
 export default store;
+
+
+
