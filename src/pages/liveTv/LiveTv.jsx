0f23e908708ac9315fb81eaf64a3fad4b2f8d096
Nimul99 <htnimul99@gmail.com>
2023-08-25 18:20:06 +0600

    layout modified in live tv page

--- src/pages/liveTv/LiveTv.jsx
+++ src/pages/liveTv/LiveTv.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import LiveVideoList from './LiveVideoList';
 import VideoPlayer from './VideoPlayer';
 
 const LiveTv = () => {
+  const [selectedChannel, setSelectedChannel] = useState(null);
+
+  useEffect(() => {
+    setSelectedChannel(channels[0]);
+  }, []);
+
+  const handleSelectChannel = (channel) => {
+    setSelectedChannel(channel);
+  };
+
+  const channels = [
+    { name: 'FOX', imageSrc: '/fox.png', videoId: 'Jcy8kYaQwkA' },
+  ];
+
   return (
-    <div className="fle flex-col items-center md:flex-row p-4">
-      <div className="w-[90%] mx-auto h-[600px] md:pr-4">
-        <VideoPlayer />
-        {/* <CommentSection /> */}
+    <div className="flex flex-col items-center md:flex-row p-4 gap-10">
+      <div className="w-full md:w-2/3 mx-auto md:pr-4"> {/* Adjust width here */}
+        <VideoPlayer channel={selectedChannel} />
       </div>
       <div className="mt-10">
-        <LiveVideoList />
+        <LiveVideoList onSelectChannel={handleSelectChannel} />
       </div>
     </div>
   );
