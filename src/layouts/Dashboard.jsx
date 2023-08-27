93049604efa08a2cbf30163265ba317e1fd4c951
Rihanul Islam <rihanthedev@gmail.com>
2023-08-28 02:08:54 +0600

    Layout: Old Dashbord Saved

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -59,7 +59,6 @@ const Dashboard = () => {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO7dCWaK60Ug98OktQFyui1Hj0EPGcWc6AvNqIx6pi&s"
               alt=""
             />
-            <h2>Antonio</h2>
           </div>
 
           <div className="divider"></div>
@@ -153,152 +152,11 @@ const Dashboard = () => {
             </Link>
           </ul>
 
-          {/* <div
-            className={`hidden lg:flex ${isSidebarOpen ? 'flex-row-reverse' : 'flex-col'
-              } mt-auto gap-2`}
-          >
-            <Link
-              className="tooltip tooltip-right flex items-center gap-2 text-sm"
-              data-tip="settings"
-            >
-              <FaRegSun size={22} /> Settings
-            </Link>
-            {user ? (
-              <Link
-                className="tooltip tooltip-right flex items-center gap-2 text-sm"
-                data-tip="sign-out"
-              >
-                <FaSignOutAlt size={22} /> Sign Out
-              </Link>
-            ) : (
-              <Link
-                className="tooltip tooltip-right flex items-center gap-2 text-sm"
-                data-tip="sign-out"
-              >
-                <FaSignInAlt size={22} /> Sign In
-              </Link>
-            )}
-          </div> */}
+          
         </div>
       </div>
 
-      {/* Dashboard Sidebar */}
-      <nav
-        className={`fixed h-full z-40 lg:hidden inset-0 bg-zinc-900 backdrop-blur-2xl text-left w-[70%] md:w-[40%] transition-all duration-300 ${isSidebarOpen ? 'block' : 'hidden lg:block'
-          }`}
-      >
-        <div className="py-5 mt-5">
-          <div className="text-white bg-zinc-800 rounded-sm mx-2 text-xl font-semibold ml-4 mb-6 flex justify-end">
-            {/* <Link onClick={handleLinkFalse} to="/dashboard">
-              <img
-                src={logo}
-                alt="Website Logo"
-                className="rounded-lg -px-2 w-24 h-24"
-              />
-            </Link> */}
-          </div>
-
-          {isAdmin ? (
-            <ul className="space-y-3 z-10">
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Admin Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/manage-users"
-                  className="block px-4 py-2  text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Manage Users
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/add-new-media"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Add New Media
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/payments"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Users Payments
-                </Link>
-              </li>
-            </ul>
-          ) : (
-            <ul className="space-y-2 text-sm md:text-base">
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/users-home"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  User Profile
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/watchlist"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  My WatchList
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/try-premium"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Try Premium
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/my-package"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  My Packages
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/payment-history"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Payment History
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={handleLinkClick}
-                  to="/dashboard/account-settings"
-                  className="block px-4 py-2 text-white bg-zinc-800 rounded-sm mx-2 hover:bg-gray-800 border-b border-cyred"
-                >
-                  Account Settings
-                </Link>
-              </li>
-            </ul>
-          )}
-        </div>
-      </nav>
+    
 
       {/* Display Page Content */}
       <div
