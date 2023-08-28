1dd6a8971c9de020f97491a1cce24a0872fd5c5a
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-28 20:34:08 +0600

    Dashboar admin routes updated

---
+++ src/Admin/manage-subscription/ManageSubscription.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManageSubscription = () => {
+  return (
+    <div>
+      <h3>Manage subscription page</h3>
+    </div>
+  )
+}
+
+export default ManageSubscription
