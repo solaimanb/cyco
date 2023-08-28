4a88d69e1b5832749a7209f542725417136b0cf3
AnnishaSiddika <annisha2112@gmail.comm>
2023-08-28 20:23:34 +0600

    getting codes

--- src/pages/DashBoard/user/GeneralUsers/GUsersHome.jsx
+++ src/pages/DashBoard/user/GeneralUsers/GUsersHome.jsx
@@ -4,6 +4,7 @@ const GUsersHome = () => {
   return (
     <div className='dashBoard-bg'>
       <h3>This is the general Users Home</h3>
+      <h3>This is the general Users Home</h3>
     </div>
   )
 }
