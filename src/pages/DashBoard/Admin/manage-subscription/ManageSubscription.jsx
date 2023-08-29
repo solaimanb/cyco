6cf85962084507fba7b303fc98ea0f4e0b0bed3b
Nimul99 <htnimul99@gmail.com>
2023-08-29 17:34:46 +0600

    layout payment gateway added in Manage Subscription for Dashboard

--- src/pages/DashBoard/Admin/manage-subscription/ManageSubscription.jsx
+++ src/pages/DashBoard/Admin/manage-subscription/ManageSubscription.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Subscription from '../../../../components/subscription/Subscription'
 
 const ManageSubscription = () => {
   return (
     <div>
-      <h3>Manage subscription page</h3>
+      <h3 className='ml-72 pl-4'>Choose a subscription</h3>
+      <div className='ml-72'>
+        <Subscription/>
+      </div>
     </div>
   )
 }
