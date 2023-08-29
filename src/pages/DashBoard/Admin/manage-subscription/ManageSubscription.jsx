4edd05358f43a1725eb45ea5d057270120762354
Nimul99 <htnimul99@gmail.com>
2023-08-29 17:39:54 +0600

    did i complated for ManageSubscription page

--- src/pages/DashBoard/Admin/manage-subscription/ManageSubscription.jsx
+++ src/pages/DashBoard/Admin/manage-subscription/ManageSubscription.jsx
@@ -4,8 +4,8 @@ import Subscription from '../../../../components/subscription/Subscription'
 const ManageSubscription = () => {
   return (
     <div>
-      <h3 className='ml-72 pl-4'>Choose a subscription</h3>
-      <div className='ml-72'>
+      <h3 className='md:ml-72 md:pl-5 lg:ml-72'>Choose a subscription</h3>
+      <div className='md:ml-72 lg:ml-72'>
         <Subscription/>
       </div>
     </div>
