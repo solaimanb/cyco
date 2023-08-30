04f63f2636c1f6063ca029556f101ebab97ce428
Rihanul Islam <rihanthedev@gmail.com>
2023-08-31 02:23:45 +0600

    Sent USer Data By Register

---
+++ src/pages/register/text.js
@@ -0,0 +1,9 @@
+const handleRegister = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const username = form.username.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const role = "user";
+
+}
