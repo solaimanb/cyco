d69b7908918f5870288a10e981cc7dbee6d7a1d6
solo <soloman.err@.com>
2023-09-01 00:23:44 +0600

    Core: category wise data fetching functionalities applied using redux

--- src/pages/Home/Categories/Categories.jsx
+++ src/pages/Home/Categories/Categories.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setCategory } from '../../../store/slices/categorySlice/categorySlice';
 import './Categories.css';
 
 const Categories = () => {
   const categoriesItem = [
-    { to: 'action-movies', name: 'Action Movies' },
-    { to: 'comedy-movies', name: 'Comedy Movies' },
-    { to: 'drama-movies', name: 'Drama Movies' },
-    { to: 'scifi-movies', name: 'SciFi Movies' },
-    { to: 'horror-movies', name: 'Horror Movies' },
-    { to: 'animated-movies', name: 'Animated Movies' },
-    { to: 'romantic-movies', name: 'Romantic Movies' },
-    { to: 'documentary-movies', name: 'Documentary Films' },
-    { to: 'tv-shows', name: 'TV Shows' },
-    { to: 'classic-movies', name: 'Classic Movies' },
-    { to: 'classic-movies', name: 'Classic Movies' },
-    { to: 'classic-movies', name: 'Classic Movies' },
+    { genre: 'Action', name: 'Action Movies' },
+    { genre: 'Comedy', name: 'Comedy Movies' },
+    { genre: 'Drama', name: 'Drama Movies' },
+    { genre: 'Sci-Fi', name: 'SciFi Movies' },
+    { genre: 'Horror', name: 'Horror Movies' },
+    { genre: 'Animation', name: 'Animation Movies' },
+    { genre: 'Romantic', name: 'Romantic Movies' },
+    { genre: 'Documentary', name: 'Documentary' },
+    { genre: 'Classic', name: 'Classic Movies' },
+    { genre: 'Adventure', name: 'Adventure Movies' },
   ];
+
+  const dispatch = useDispatch();
+
+  const handleCategoryClick = (category) => {
+    dispatch(setCategory(category?.genre));
+  };
+
   return (
     <div className="sticky top-24 mt-14">
       <ul className="space-y-3">
         {categoriesItem.map((category, index) => (
-          <Link
-            to={category?.to}
+          <button
             role="button"
+            onClick={() => handleCategoryClick(category)}
             className="cyco-btn btn w-full h-full py-1 bg-zinc-800 border border-gray-200 rounded-sm"
             key={index}
           >
             {category?.name}
-          </Link>
+          </button>
         ))}
       </ul>
     </div>
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import homeSlice from './homeSlice/homeSlice';
-import userReducer from './userSlice/userSlice';
-import wishlistReducer from './wishListSlice/wishListSlice'; // Renamed the reducer
+import categoryReducer from './slices/categorySlice/categorySlice';
+import homeSlice from './slices/homeSlice/homeSlice';
+import userReducer from './slices/userSlice/userSlice';
+import wishlistReducer from './slices/wishListSlice/wishListSlice';
 
 const store = configureStore({
   reducer: {
     home: homeSlice,
     users: userReducer,
     wishlist: wishlistReducer,
+    category: categoryReducer,
   },
 });
 
