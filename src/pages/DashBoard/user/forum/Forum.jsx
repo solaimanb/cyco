259fbeec0fe10fd4708fe624972486f81b43909b
solo <soloman.err@.com>
2023-09-02 01:23:24 +0600

    Layout: Subscription page modified

--- src/pages/DashBoard/user/forum/Forum.jsx
+++ src/pages/DashBoard/user/forum/Forum.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Forum = () => {
     return (
-        <div className='h-screen'>
+        <div className='min-h-screen'>
             <p className="border-l-4 border-white mt-5 ml-5 inline-block px-5">Cyco Forum</p>
         </div>
     );
