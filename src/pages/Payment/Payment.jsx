4c0f60f30c8b5b34f99de2aa532c08b6e333a579
solo <soloman.err@.com>
2023-09-02 15:54:53 +0600

    UT: unit testing execution integrated using JEST

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,70 +1,65 @@
-import React from "react";
-import { FaFulcrum } from "react-icons/fa";
-import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import React from 'react';
+import { FaFulcrum } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
-import "./payment.css";
-import { Elements } from "@stripe/react-stripe-js";
-import { CheckoutForm } from "./Checkout";
+import { CheckoutForm } from './Checkout';
+import './payment.css';
 // import { CheckoutForm } from "./Checkout";
 
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 
 const Payment = () => {
-
   const { selectedPlan, amount } = useSelector((state) => state.payment);
   // const price = parseFloat(amount.tofixed(2))
-  const price = amount
+  const price = amount;
 
   // Function to format the date as "MMM DD, YYYY"
   const formatDate = (date) => {
-    const options = { year: "numeric", month: "short", day: "numeric" };
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
     return new Date(date).toLocaleDateString(undefined, options);
   };
 
   const currentDate = formatDate(new Date());
 
   return (
-    <>
-      <div className=" relative flex justify-center items-center min-h-screen backdrop-filter backdrop-blur-sm opacity-90 ">
-        <div className="absolute top-20 animate-pulse">
-          <FaFulcrum className="text-4xl text-cyred" />
-          <div>
-            <h3>
-              Onetime Subscription{" "}
-              <span className="text-cyred text-xl md:text-3xl">Lifetime</span>{" "}
-              Achievements
-            </h3>
-          </div>
+    <div className=" relative flex justify-center items-center min-h-screen backdrop-filter backdrop-blur-sm opacity-90 ">
+      <div className="absolute top-20 animate-pulse">
+        <FaFulcrum className="text-4xl text-cyred" />
+        <div>
+          <h3>
+            Onetime Subscription{' '}
+            <span className="text-cyred text-xl md:text-3xl">Lifetime</span>{' '}
+            Achievements
+          </h3>
         </div>
-        <div className="absolute bottom-20 ">
-          <div>
-            <h3>
-              <span className="text-cyred text-xl md:text-3xl animate-pulse">
-                Subscribe
-              </span>{" "}
-              & Get Free Access of{" "}
-              <span className="text-cyred text-xl md:text-3xl">VIP</span> club{" "}
-            </h3>
-          </div>
-          <div className="flex justify-end ">
-            <div>
-          
-            </div>
-            <FaFulcrum className="text-4xl text-cyred " />
-          </div>
+      </div>
+      <div className="absolute bottom-20 ">
+        <div>
+          <h3>
+            <span className="text-cyred text-xl md:text-3xl animate-pulse">
+              Subscribe
+            </span>{' '}
+            & Get Free Access of{' '}
+            <span className="text-cyred text-xl md:text-3xl">VIP</span> club{' '}
+          </h3>
         </div>
-        <div id="borderAnimation">
-          <div className="z-20">
-           <div>
-           <p className="-mt-16 mb-16">Pay Amount {price}</p>
-           <h2 className="text-2xl font-semibold text-cyred mb-6 md:mb-12 -rotate-1">
+        <div className="flex justify-end ">
+          <div></div>
+          <FaFulcrum className="text-4xl text-cyred " />
+        </div>
+      </div>
+      <div id="borderAnimation">
+        <div className="z-20">
+          <div>
+            <p className="-mt-16 mb-16">Pay Amount {price}</p>
+            <h2 className="text-2xl font-semibold text-cyred mb-6 md:mb-12 -rotate-1">
               Payment Essentials
             </h2>
-           
-           </div>
+          </div>
 
-            {/* <form className=""> */}
-              {/* <div className="mb-4">
+          {/* <form className=""> */}
+          {/* <div className="mb-4">
               <label className=" block text-sm font-medium text-gray-700">
                 Card Number
               </label>
@@ -94,16 +89,14 @@ const Payment = () => {
               </div>
             </div> */}
 
-              <Elements stripe={stripePromise}>
-                <CheckoutForm price={price} />
-              </Elements>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm price={price} />
+          </Elements>
 
-              
-            {/* </form> */}
-          </div>
+          {/* </form> */}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ src/utils/tests/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./sum');
+
+test('adds two value', () => {
+  expect(sum(1, 3)).toBe(4);
+});
