a0eb2ec49487b7e53acda2971a9ee42c61f6b038
Rihanul Islam <rihanthedev@gmail.com>
2023-09-09 16:31:09 +0600

    remvoed old Party

--- src/pages/DashBoard/user/watch-party/WatchParty.jsx
+++ src/pages/DashBoard/user/watch-party/WatchParty.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import useParty from '../../../../hooks/useParty';
+import PartyCard from './partyCard/PartyCard';
+import { Link } from 'react-router-dom';
+import Marquee from 'react-fast-marquee';
+import Loading from '../../../../components/loading/Loading';
 
 const WatchParty = () => {
+
     return (
         <div>
-            hi
+            <h2>Watch Party Comming Soooooon</h2>
         </div>
     );
 };
