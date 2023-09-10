c5d6d348f11589b4c3b8adf08f12692796b4b497
solo <soloman.err@.com>
2023-09-10 14:34:15 +0600

    wishlist routes bugs & api conflict resolved

--- src/pages/DashBoard/user/wishlist/WishList.jsx
+++ src/pages/DashBoard/user/wishlist/WishList.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import { AuthContext } from '../../../../providers/AuthProvider';
 import WishCard from './WishCard';
 
-
 const Wishlist = () => {
   const [axiosSecure] = useAxiosSecure();
-  const { user, loading } = useContext(AuthContext)
+  const { user, loading } = useContext(AuthContext);
   const [wishlist, setWishlist] = useState([]) || [];
   console.log(wishlist);
   // if(loading){
@@ -26,23 +26,66 @@ const Wishlist = () => {
         });
     }
   }, [user, axiosSecure]);
-  
+
+  // REMOVE FROM WISHLIST:
+  const handleRemoveFromWishlist = async (movieId) => {
+    try {
+      if (user) {
+        const response = await Swal.fire({
+          text: 'Want to delete from wishlist?',
+          icon: 'warning',
+          background: '#222',
+          showCancelButton: true,
+          confirmButtonText: 'Remove',
+          cancelButtonText: 'Cancel',
+        });
+
+        if (response.isConfirmed) {
+          const response = await axiosSecure.delete(
+            `/wishlist/${user?.email}/${movieId}`
+          );
+
+          if (response?.status === 200) {
+            setWishlist((prevWishlist) =>
+              prevWishlist?.filter((movie) => movie?._id !== movieId)
+            );
+
+            Swal.fire({
+              text: 'The movie has been removed from your wishlist!',
+              icon: 'success',
+              background: '#222',
+            });
+          } else {
+            console.log(
+              'Error removing movie from wishlist:',
+              response?.data?.error
+            );
+          }
+        }
+      }
+    } catch (error) {
+      console.log('Error removing movie from wishlist', error);
+    }
+  };
 
   return (
-    <div className="container h-screen mx-auto mt-4">
-      <h2 className="font-semibold mb-4">My Wishlist</h2>
-      {wishlist && wishlist.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {wishlist.map((movie) => (
+    <section className="min-h-screen p-2 md:p-3 mt-3 lg:mt-0 backdrop-blur-sm bg-zinc-950">
+      <p className="hidden md:flex text-sm md:text-base font-semibold border-l-4 border-cyred ml-2 px-2 md:px-5">
+        My wishlist
+      </p>
+
+      {wishlist && wishlist?.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
+          {wishlist?.map((movie) => (
             <WishCard
-              key={movie.id}
+              key={movie?._id}
               movie={movie}
-              onRemove={() => handleRemoveFromWishlist(movie)}
+              onRemove={() => handleRemoveFromWishlist(movie?._id)}
             />
           ))}
         </div>
       ) : (
-        <p className='text-2xl text-center'>No movies in your wishlist.</p>
+        <p className="text-2xl text-center">No movies in your wishlist.</p>
       )}
 
       {/* <div className="flex justify-center mt-4">
@@ -60,38 +103,8 @@ const Wishlist = () => {
           </button>
         ))}
       </div> */}
-    </div>
+    </section>
   );
-  //   return (
-  //     // <div className="container mx-auto mt-4">
-  //     //   <h2 className="text-2xl font-semibold mb-4">My Wishlist</h2>
-  //     //   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-  //     //     {/* {moviesForCurrentPage.map((movie) => (
-  //     //       <WatchListCard
-  //     //         key={movie.id}
-  //     //         movie={movie}
-  //     //         onRemove={() => handleRemoveFromWishlist(movie)}
-  //     //       /> */}
-  //     //     {/* ))} */}
-  //     //   </div>
-
-  //     //   <div className="flex justify-center mt-4">
-  //     //     {Array.from({ length: totalPages }, (_, index) => (
-  //     //       {/* <button
-  //     //         key={index + 1}
-  //     //         onClick={() => goToPage(index + 1)}
-  //     //         className={`mx-2 px-2 py-1 rounded ${
-  //     //           currentPage === index + 1
-  //     //             ? "bg-cyred text-white"
-  //     //             : "bg-white text-cyred hover:bg-gray-200"
-  //     //         }`}
-  //     //       >
-  //     //         {index + 1}
-  //     //       </button> */}
-  //     //     ))}
-  //     //   </div>
-  //     // </div>
-  //   );
 };
 
 export default Wishlist;
