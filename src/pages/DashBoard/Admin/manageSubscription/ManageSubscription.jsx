dcf5cb981853898b7ad7b97cfdbb7147f5a7819f
Md Sarowar <mdsarowarhang@gmail.com>
2023-09-13 12:04:04 +0300

    In the admin-dashboard routes 'Manage subscription' page, make functionalities for update/upload/edit subscription tier cards. then save them in the database.

--- src/pages/DashBoard/Admin/manageSubscription/ManageSubscription.jsx
+++ src/pages/DashBoard/Admin/manageSubscription/ManageSubscription.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Subscription from "../../../../components/subscription/Subscription";
 import ManageSubscriptionFirst from "../../../../shared/manageSubscriptionShared/ManageSubscriptionFirst";
 import {
@@ -10,103 +10,57 @@ import {
   Button,
   useDisclosure,
 } from "@nextui-org/react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
+import { fetchItems } from "../../../../store/slices/subscriptionSlice/subscriptionSlice";
 const ManageSubscription = () => {
-   const items = useSelector((state) => state.manageSubscriptions)
+  const items = useSelector((state) => state.manageSubscriptions);
   const [isDataBasic, setIsDataBasic] = useState();
-  console.log(isDataBasic);
-  const [isDataStandard, setIsDataStandard] = useState();
-  const [isDataMostPopular, setIsDataMostPopular] = useState();
-  const [isDataUltaPremium, setIsDataUltaPremium] = useState();
-  
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchItems());
+  }, [dispatch, items]);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
- ;
-  console.log(items[0]);
-  const handlePassDataBasic = (items) => {
-    setIsDataBasic(items)
-    console.log(items);
-  };
-  const handlePassDataStandard = (items) => {
-    setIsData(items);
-    console.log(items);
-  };
-  const handlePassDataMostPopular = (items) => {
-    setIsData(items);
-    console.log(items);
-  };
-  const handlePassDataUltaPremium = (items) => {
-    setIsData(items);
-    console.log(items);
+  const handlePassDataBasic = (id) => {
+    setIsDataBasic(id);
   };
   return (
     <>
       <div className="min-h-screen items-center ">
         <h1 className=" text-3xl text-center m-5">
-          Don't come to this page because I am still working on this page.
+          Manage Subscription for Admin
         </h1>
 
         {/* 1nd number card */}
-        <div className="modal-box max-w-md mx-auto bg-white p-4 rounded shadow-lg text-gray-700">
-          <Button
-            onClick={() => handlePassDataBasic(items[0])}
-            onPress={onOpen}
-            className="btn btn-sm  absolute right-2 top-2"
-            color="secondary"
-          >
-            Edit
-          </Button>
-          <div className="">
-            <h2 className="text-2xl font-semibold mb-4">Basic</h2>
-          </div>
-        </div>
-
-        {/* 2nd number card */}
-        <div className="modal-box max-w-md mx-auto bg-white p-4 rounded shadow-lg text-gray-700">
-          <Button
-            onClick={''}
-            onPress={onOpen}
-            className="btn btn-sm  absolute right-2 top-2"
-            color="secondary"
-          >
-            Edit
-          </Button>
-          <div className="">
-            <h2 className="text-2xl font-semibold mb-4">Most Popular</h2>
-          </div>
-        </div>
-        {/* 3nd number card */}
-        <div className="modal-box max-w-md mx-auto bg-white p-4 rounded shadow-lg text-gray-700">
-          <Button
-            onPress={onOpen}
-            className="btn btn-sm  absolute right-2 top-2"
-            color="secondary"
-          >
-            Edit
-          </Button>
-          <div className="">
-            <h2 className="text-2xl font-semibold mb-4">Ulta Premium</h2>
-          </div>
-        </div>
-        {/* 4nd number card */}
-        <div className="modal-box max-w-md mx-auto bg-white p-4 rounded shadow-lg text-gray-700">
-          <Button
-            onPress={onOpen}
-            className="btn btn-sm absolute right-2 top-2"
-            color="secondary"
-          >
-            Edit
-          </Button>
-          <div className="">
-            <h2 className="text-2xl font-semibold mb-4">Standard</h2>
-          </div>
-        </div>
+        {items &&
+          items.map((item) => (
+            <div
+              item={item}
+              key={item?._id}
+              className="modal-box max-w-md mx-auto bg-white p-4 rounded shadow-lg text-gray-700"
+            >
+              <Button
+                onClick={() => handlePassDataBasic(item)}
+                onPress={onOpen}
+                className="btn btn-sm  absolute right-2 top-2"
+                color="secondary"
+              >
+                Edit
+              </Button>
+              <div className="">
+                <h2 className="text-2xl font-semibold mb-4">{item?.name}</h2>
+              </div>
+            </div>
+          ))}
       </div>
-     
+
       <ManageSubscriptionFirst
-        passData={isDataBasic}
+        items={isDataBasic}
         onOpenChange={onOpenChange}
         isOpen={isOpen}
+        
       />
     </>
   );
