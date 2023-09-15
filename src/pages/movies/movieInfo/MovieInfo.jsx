d9d99f6fddb623ec260521a423e6f8c0f36a195f
kabir shuvo <kabirshuvo19@gmail.com>
2023-09-15 11:40:26 +0600

    Bug fixed to movieReviews component,

--- src/pages/movies/movieInfo/MovieInfo.jsx
+++ src/pages/movies/movieInfo/MovieInfo.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Marquee from "react-fast-marquee";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { LuListVideo } from "react-icons/lu";
