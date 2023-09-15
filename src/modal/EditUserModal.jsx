16be640c9ba76996dd8ba372039fc655b4e7faa4
Md Sarowar <mdsarowarhang@gmail.com>
2023-09-15 14:17:52 +0300

    Manage user profile edit functionality.

--- src/modal/EditUserModal.jsx
+++ src/modal/EditUserModal.jsx
@@ -1,111 +1,151 @@
-import { Dialog, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
-import { useForm } from 'react-hook-form';
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useEffect } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { updateData } from "../store/slices/editUserSlice/editUserSlice";
+import useAuth from "../hooks/useAuth";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-const EditUserModal = ({closeModal, isOpen}) => {
-    
-    const { register, handleSubmit, setValue } = useForm({
-        defaultValues: {
-         
-        
-         
-        }
-      });
-      // Submit your data into Redux store
-      const onSubmit = data => {
-        updateSubscription(data, items?._id)
-        .then(data => {
-          Swal.fire({
-            icon: 'success',
-            title: 'Update Successful',
-            showConfirmButton: false,
-            timer: 1500
-          })
-          
-   
-
-        
-        })
-        .catch(err => {
-          console.log(err)
-        
-        })
-       
-      }
+const EditUserModal = ({ closeModal, isOpen }) => {
+  const {user} = useAuth()
+  const [isData, setIsData] = useState()
+  console.log(isData);
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {},
+  });
+  const dispatch = useDispatch();
+  // Submit your data into Redux store
+  const handleUpdateData = (email, data) => {
+    console.log(email,'hh', data);
+    dispatch(updateData({email, data}));
+  };
+  const onSubmit = (data) => {
+    setIsData(data)
+    console.log(data);
+  };
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <Transition.Child
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
-                  as='h3'
-                  className='text-lg font-medium leading-6 text-gray-900'
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
                 >
-                 Edit Profile
+                  Edit Profile
                 </Dialog.Title>
-                <div className='mt-2'>
-              
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name:</label>
-          <input  className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" {...register("name")} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field1" className="block text-sm font-medium text-gray-700">Email</label>
-          <input defaultValue={''} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="email" {...register("email", { min: 1, max: 12 })} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field2" className="block text-sm font-medium text-gray-700">Month Free</label>
-          <input defaultValue={''} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("month_free",)} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field3" className="block text-sm font-medium text-gray-700">Save</label>
-          <input defaultValue={''} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("save", { min: 20, max: 70 })} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field4" className="block text-sm font-medium text-gray-700">Profit</label>
-          <input defaultValue={''} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("profit", )} />
-        </div>
-        </form>
-                </div>
-                <hr className='mt-8 ' />
-                <div className='flex mt-2 justify-around'>
-                  <button
-                    type='button'
-                    className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
-                    
-                  >
-                    Submit
-                  </button>
-                  <button
-                    type='button'
-                    className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
-                    onClick={closeModal}
-                  >
-                    Cancel
-                  </button>
+                <div className="mt-2">
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="username"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        User Name:
+                      </label>
+                      <input
+                        className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300"
+                        {...register("username")}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Email
+                      </label>
+                      <input
+                        disabled
+                        defaultValue={""}
+                        className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300"
+                        type="email"
+                        {...register("email")}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="phoneNumber"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Phone Number
+                      </label>
+                      <input
+                        defaultValue={""}
+                        className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300"
+                        type="number"
+                        {...register("phoneNumber")}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="photo">Upload Photo:</label>
+                      <Controller
+                        name="photo"
+                        control={control}
+                        render={({ field }) => (
+                          <input
+                            className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300"
+                            type="file"
+                            id="photo"
+                            {...field}
+                            accept="image/*" // Specify accepted file types (images in this case)
+                          />
+                        )}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Address
+                      </label>
+                      <input
+                        defaultValue={""}
+                        className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300"
+                        type="text"
+                        {...register("address")}
+                      />
+                    </div>
+                    <hr className="mt-8 " />
+                    <div className="flex mt-2 justify-around">
+                      <input
+                      onClick={()=>handleUpdateData(user?.email, isData)}
+                        type="Submit"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      />
+
+                      <button
+                        type="button"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                        onClick={closeModal}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
@@ -113,7 +153,7 @@ const EditUserModal = ({closeModal, isOpen}) => {
         </div>
       </Dialog>
     </Transition>
-  )
-}
+  );
+};
 
-export default EditUserModal
+export default EditUserModal;
--- src/shared/manageSubscriptionShared/ManageSubscriptionFirst.jsx
+++ src/shared/manageSubscriptionShared/ManageSubscriptionFirst.jsx
@@ -1,111 +1,119 @@
 import React, { useEffect, useState } from 'react';
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from "react-hook-form";
 
 import { fetchItems, updateItemRequest } from '../../store/slices/subscriptionSlice/subscriptionSlice';
 import { updateSubscription } from '../../api/updateSubscription';
 import Swal from 'sweetalert2';
-const ManageSubscriptionFirst = ({isOpen, onOpenChange, items}) => {
- console.log(items);
-    
-      const { register, handleSubmit, setValue } = useForm({
-        defaultValues: {
-         
-          month: items?.month,
-          month_free: items?.month_free,
-           save: items?.save,
-          profit: items?.profit,
-         
-        }
-      });
-      // Submit your data into Redux store
-      const onSubmit = data => {
-        updateSubscription(data, items?._id)
-        .then(data => {
-          Swal.fire({
-            icon: 'success',
-            title: 'Update Successful',
-            showConfirmButton: false,
-            timer: 1500
-          })
-          
-   
 
-        
+const ManageSubscriptionFirst = ({ isOpen, onOpenChange, items }) => {
+  console.log(items);
+
+  const { register, handleSubmit, setValue } = useForm({
+ 
+  });
+
+  // Submit your data into Redux store
+  const onSubmit = data => {
+    updateSubscription(data, items?._id)
+      .then(data => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Update Successful',
+          showConfirmButton: false,
+          timer: 1500
         })
-        .catch(err => {
-          console.log(err)
-        
-        })
-       
-      }
-    return (
-       
-           
-      <Modal 
-        backdrop="opaque" 
-        isOpen={isOpen} 
-        onOpenChange={onOpenChange}
-        radius="2xl"
-        classNames={{
-          body: "py-6",
-          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
-          base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
-          header: "border-b-[1px] border-[#292f46]",
-          footer: "border-t-[1px] border-[#292f46]",
-          closeButton: "hover:bg-white/5 active:bg-white/10",
-        }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Manage Subscriptions</ModalHeader>
-              
-              <ModalBody>
-              <div className="modal-box max-w-md mx-auto p-4 rounded shadow-lg text-gray-700">
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <Modal
+      backdrop="opaque"
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      radius="2xl"
+      classNames={{
+        body: "",
+        backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
+        base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+        header: "border-b-[1px] border-[#292f46]",
+        footer: "border-t-[1px] border-[#292f46]",
+        closeButton: "hover:bg-white/5 active:bg-white/10",
+      }}
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Manage Subscriptions</ModalHeader>
+
+            <ModalBody>
+              <div className="modal-box w-full mx-auto p-4 rounded shadow-lg">
+
+                <div className="">
+                  <h2 className="text-2xl font-semibold mb-4">Subscription Form</h2>
+                  <form className='grid ' onSubmit={handleSubmit(onSubmit)}>
+                  <div className='flex gap-2'>
+                      <div className="mb-4">
+                      <label htmlFor="title" className=" text-sm font-medium text-gray-700">Title</label>
+                      <input defaultValue={items?.title} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="text" {...register("title")} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="price" className="block text-sm font-medium text-gray-700">Price</label>
+                      <input defaultValue={items?.price} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("price",)} />
+                    </div>
+                  </div>
+                  <div className='flex gap-2'>
+  <div className="mb-4">
+                      <label htmlFor="save_price" className="block text-sm font-medium text-gray-700">save_price</label>
+                      <input defaultValue={items?.save_price} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("save_price")} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="previous_pay" className="block text-sm font-medium text-gray-700">previous_pay</label>
+                      <input defaultValue={items?.previous_pay} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("previous_pay")} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="months_free" className="block text-sm font-medium text-gray-700">Months free</label>
+                      <input defaultValue={items?.months_free} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("months_free")} />
+                    </div>
+                  </div>
     
-      <div className="">
-      <h2 className="text-2xl font-semibold mb-4">Subscription Form</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name:</label>
-          <input defaultValue={items?.name} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" {...register("name")} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field1" className="block text-sm font-medium text-gray-700">Month</label>
-          <input defaultValue={items?.month} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("month", { min: 1, max: 12 })} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field2" className="block text-sm font-medium text-gray-700">Month Free</label>
-          <input defaultValue={items?.month_free} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("month_free",)} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field3" className="block text-sm font-medium text-gray-700">Save</label>
-          <input defaultValue={items?.save} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("save", { min: 20, max: 70 })} />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="field4" className="block text-sm font-medium text-gray-700">Profit</label>
-          <input defaultValue={items?.profit} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="number" {...register("profit", )} />
-        </div>
-        <ModalFooter>
-                <Button color="foreground" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <input className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20" type="submit" />
-               
-              </ModalFooter>
-      </form>
-    </div>
-  </div>
-              
-             
-           </ModalBody> </>
-          )}
-        </ModalContent>
-      </Modal>
-    
-    );
+                    
+                    {/* Add new input fields here */}
+                    <div className="mb-4">
+                      <label htmlFor="feature1" className="block text-sm font-medium text-gray-700">Feature-1</label>
+                      <input defaultValue={items?.feature1} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="text" {...register("feature1")} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="feature2" className="block text-sm font-medium text-gray-700">Feature-2</label>
+                      <input defaultValue={items?.feature2} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="text" {...register("feature2")} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="feature3" className="block text-sm font-medium text-gray-700">Feature-3</label>
+                      <input defaultValue={items?.feature3} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="text" {...register("feature3")} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="feature4" className="block text-sm font-medium text-gray-700">Feature-4</label>
+                      <input defaultValue={items?.feature4} className="mt-1 p-2 w-full text-white border rounded focus:outline-none focus:ring focus:border-blue-300" type="text" {...register("feature4")} />
+                    </div>
+                    <ModalFooter>
+                      <Button color="foreground" variant="light" onPress={onClose}>
+                        Close
+                      </Button>
+                      <input className="bg-[#6f4ef2] btn rounded-lg shadow-lg shadow-indigo-500/20" type="submit" />
+                    </ModalFooter>
+                  </form>
+                </div>
+              </div>
+            </ModalBody>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default ManageSubscriptionFirst;
