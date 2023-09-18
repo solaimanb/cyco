d5420b60383d290cd5de38c7467d3e424c8fe4e1
solo <soloman.err@.com>
2023-09-18 20:38:09 +0600

    Fix: routes/import conflict resolved

--- src/pages/notify/ReceiveNotification.jsx
+++ src/pages/notify/ReceiveNotification.jsx
@@ -1,62 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
+// import React, { useEffect, useState } from 'react';
+// import io from 'socket.io-client';
 
-const socket = io.connect(`${import.meta.env.VITE_SERVER_URL}`);
+// const socket = io.connect(`${import.meta.env.VITE_SERVER_URL}`);
 
-const ReceiveNotification = () => {
-  const [nCount, setNCount] = useState(0);
-  const [receiveNotification, setReceiveNotification] = useState('');
-  const [showNotification, setShowNotification] = useState(false);
-  const [notificationHistory, setNotificationHistory] = useState([]);
+// const ReceiveNotification = () => {
+//   const [nCount, setNCount] = useState(0);
+//   const [receiveNotification, setReceiveNotification] = useState('');
+//   const [showNotification, setShowNotification] = useState(false);
+//   const [notificationHistory, setNotificationHistory] = useState([]);
 
-  // Load notification history from local storage on component mount
-  useEffect(() => {
-    const savedNotificationHistory = JSON.parse(localStorage.getItem('notificationHistory'));
-    if (savedNotificationHistory) {
-      setNotificationHistory(savedNotificationHistory);
-    }
+//   // Load notification history from local storage on component mount
+//   useEffect(() => {
+//     const savedNotificationHistory = JSON.parse(localStorage.getItem('notificationHistory'));
+//     if (savedNotificationHistory) {
+//       setNotificationHistory(savedNotificationHistory);
+//     }
 
-    socket.on('receive_notification', (data) => {
-      setReceiveNotification(data.notification);
-      setNCount((prevCount) => prevCount + 1);
-      setShowNotification(showNotification);
+//     socket.on('receive_notification', (data) => {
+//       setReceiveNotification(data.notification);
+//       setNCount((prevCount) => prevCount + 1);
+//       setShowNotification(showNotification);
       
-      // Update notification history and store it in local storage
-      const updatedHistory = [data.notification, ...notificationHistory];
-      setNotificationHistory(updatedHistory);
-      localStorage.setItem('notificationHistory', JSON.stringify(updatedHistory));
-    });
-  }, [socket]);
+//       // Update notification history and store it in local storage
+//       const updatedHistory = [data.notification, ...notificationHistory];
+//       setNotificationHistory(updatedHistory);
+//       localStorage.setItem('notificationHistory', JSON.stringify(updatedHistory));
+//     });
+//   }, [socket]);
 
-  const handleShowNotificationClick = () => {
-    setShowNotification(!showNotification);
-  };
+//   const handleShowNotificationClick = () => {
+//     setShowNotification(!showNotification);
+//   };
 
-  return (
-    <div className='h-screen flex flex-col items-center justify-center text-white'>
-      <div className='bg-sky-700 px-16 py-16 rounded w-1/4'>
-        {showNotification ? (
-          <p className='text-cyred'>{receiveNotification}</p>
-        ) : (
-          <p className='text-cyred'>{nCount}</p>
-        )}
-        {/* Display previous notifications only when showNotification is true */}
-        {showNotification && (
-          <ul>
-            {notificationHistory.map((notification, index) => (
-              <li key={index}>{notification}</li>
-            ))}
-          </ul>
-        )}
-      </div>
+//   return (
+//     <div className='h-screen flex flex-col items-center justify-center text-white'>
+//       <div className='bg-sky-700 px-16 py-16 rounded w-1/4'>
+//         {showNotification ? (
+//           <p className='text-cyred'>{receiveNotification}</p>
+//         ) : (
+//           <p className='text-cyred'>{nCount}</p>
+//         )}
+//         {/* Display previous notifications only when showNotification is true */}
+//         {showNotification && (
+//           <ul>
+//             {notificationHistory.map((notification, index) => (
+//               <li key={index}>{notification}</li>
+//             ))}
+//           </ul>
+//         )}
+//       </div>
 
-      <div className='my-4'>
-        <button onClick={handleShowNotificationClick} className='bg-sky-700 px-8 py-1 rounded-lg'>
-          {showNotification ? 'Close Notification' : 'Show Notification'}
-        </button>
-      </div>
-    </div>
-  );
-};
+//       <div className='my-4'>
+//         <button onClick={handleShowNotificationClick} className='bg-sky-700 px-8 py-1 rounded-lg'>
+//           {showNotification ? 'Close Notification' : 'Show Notification'}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ReceiveNotification;
+// export default ReceiveNotification;
