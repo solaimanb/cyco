a11428f0215c9259123fd054dab0dfd8fe10d890
solo <soloman.err@.com>
2023-09-18 18:48:26 +0600

    Backup: 'bin' added to backup the previous version

--- src/pages/notify/ReceiveNotification.jsx
+++ src/pages/notify/ReceiveNotification.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 const socket = io.connect(`${import.meta.env.VITE_SERVER_URL}`);
 
