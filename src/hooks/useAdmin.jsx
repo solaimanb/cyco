cf56416c42259afb88f65117f9382c2991eb1339
AnnishaSiddika <annisha2112@gmail.comm>
2023-09-22 20:06:24 +0600

    edit user profile implemented

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,19 +4,27 @@ import useAxiosSecure from './useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 
 const useAdmin = () => {
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const [axiosSecure] = useAxiosSecure();
+
     const { data: isAdmin, isLoading: isAdminLoading } = useQuery(
         ['isAdmin', user?.email],
         async () => {
-          const res = await axiosSecure.get(`/users/admin/${user?.email}`);
-          return res.data.admin;
+            try {
+                const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+                return res.data.admin;
+            } catch (error) {
+                // Handle the error gracefully, e.g., set isAdmin to false
+                return false;
+            }
         },
         {
-          enabled: !loading,
+            enabled: !loading,
+            defaultValue: false, // Default value when loading or error occurs
         }
-      );
-    return [ isAdmin, isAdminLoading];
+    );
+
+    return [isAdmin, isAdminLoading];
 };
 
 export default useAdmin;
--- src/pages/Home/hero/Hero.jsx
+++ src/pages/Home/hero/Hero.jsx
@@ -69,7 +69,7 @@ const Hero = () => {
 
   return (
     
-      <section className="relative h-[70vh] md:h-[85vh] lg:h-screen xl:h-[90vh] pt-10 2xl:pt-28 overflow-hidden bg-black/60  m-0 p-0">
+      <section className="relative h-[70vh] md:h-[85vh] lg:h-screen xl:h-[90vh] 2xl:pt-28 overflow-hidden bg-black/60  m-0 p-0">
         {/* keep the CanvasAnimation under this section do not change this again */}
         <CanvasAnimation>
         <Swiper
